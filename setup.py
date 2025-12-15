97ddb79fda4abdd3b6d51a4b2346aa1377c6b0bc
Shafin Ahmed <84165820+Shafin2954@users.noreply.github.com>
2025-12-16 00:38:22 +0600

    Update setup.py
    Updated the version so that pip installs the newer version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="rps-game-cli",
-    version="1.0.0",
+    version="1.0.1",
     author="Sohanur Rahman Sohan",
     description="A fun Rock Paper Scissors game for the terminal",
     long_description=long_description,
